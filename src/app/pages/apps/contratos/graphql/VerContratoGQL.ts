a4685ee2e2411d7015ee5385839ed33be0a3f06b
shoniisrael <jivillacis4@espe.edu.ec>
2020-08-27 00:30:40 -0500

    Corrijo error merge

--- src/app/pages/apps/contratos/graphql/VerContratoGQL.ts
+++ src/app/pages/apps/contratos/graphql/VerContratoGQL.ts
@@ -1,5 +1,5 @@
 import { Injectable} from "@angular/core";
-import {Query} from 'apollo-angular';
+import {Mutation} from 'apollo-angular';
 import gql from "graphql-tag";
 
 import { Contrato } from 'src/app/models/contratoalquiler.model';
