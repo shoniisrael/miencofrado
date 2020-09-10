d757a8c50cf12c1c6180961ae1481f4e03369944
shoniisrael <jivillacis4@espe.edu.ec>
2020-09-09 22:57:24 -0500

    recupero clientes

--- src/app/pages/dashboards/dashboard-analytics/dashboard-analytics.component.ts
+++ src/app/pages/dashboards/dashboard-analytics/dashboard-analytics.component.ts
@@ -1,83 +1,148 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import icGroup from '@iconify/icons-ic/twotone-group';
-import icPageView from '@iconify/icons-ic/twotone-pageview';
-import icCloudOff from '@iconify/icons-ic/twotone-cloud-off';
-import icTimer from '@iconify/icons-ic/twotone-timer';
-import { defaultChartOptions } from '../../../../@vex/utils/default-chart-options';
-import { Order, tableSalesData } from '../../../static-data/table-sales-data';
-import { TableColumn } from '../../../../@vex/interfaces/table-column.interface';
-import icMoreVert from '@iconify/icons-ic/twotone-more-vert';
+import { ChangeDetectorRef, Component, OnInit,Input } from "@angular/core";
+import icGroup from "@iconify/icons-ic/twotone-group";
+import icPageView from "@iconify/icons-ic/twotone-pageview";
+import icCloudOff from "@iconify/icons-ic/twotone-cloud-off";
+import icTimer from "@iconify/icons-ic/twotone-timer";
+import { defaultChartOptions } from "../../../../@vex/utils/default-chart-options";
+import { Order, tableSalesData } from "../../../static-data/table-sales-data";
+import { TableColumn } from "../../../../@vex/interfaces/table-column.interface";
+import icMoreVert from "@iconify/icons-ic/twotone-more-vert";
+
+
+import { Observable, ReplaySubject } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { filter } from 'rxjs/operators';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
+
+//Modelo
+import { Cliente } from "src/app/models/cliente.model";
+import { queryGetClientes } from "src/app/services/clientes";
+import { DeleteClienteGQL } from "../../apps/clientes/graphql/DeleteClienteGQL";
+import { CustomerCreateUpdateComponent } from "../../apps/clientes/customer-create-update/customer-create-update.component";
+import { MatTableDataSource } from '@angular/material/table';
+
+export type Query = {
+  cliente: Cliente[];
+};
+const GetClientes = queryGetClientes;
 
 @Component({
-  selector: 'vex-dashboard-analytics',
-  templateUrl: './dashboard-analytics.component.html',
-  styleUrls: ['./dashboard-analytics.component.scss']
+  selector: "vex-dashboard-analytics",
+  templateUrl: "./dashboard-analytics.component.html",
+  styleUrls: ["./dashboard-analytics.component.scss"],
 })
 export class DashboardAnalyticsComponent implements OnInit {
+  subject$: ReplaySubject<Cliente[]> = new ReplaySubject<Cliente[]>(1);
+  data$: Observable<Cliente[]> = this.subject$.asObservable();
+  data: Observable<Cliente[]>;
+  customers: Cliente[];
+
+  
+  @Input()
+  columns: TableColumn<Cliente>[] = [
+    {
+      label: 'Checkbox',
+      property: 'checkbox',
+      type: 'checkbox',
+      visible: true,
+    },
+    { label: 'cedula', property: 'cedula', type: 'text', visible: true },
+    { label: 'nombre', property: 'nombre', type: 'text', visible: true },
+    { label: 'nombre2', property: 'nombre2', type: 'text', visible: true },
+    { label: 'email', property: 'email', type: 'text', visible: false },
+    { label: 'telf1', property: 'telf1', type: 'text', visible: true },
+    { label: 'telf2', property: 'telf2', type: 'text', visible: true },
+    { label: 'telf3', property: 'telf3', type: 'text', visible: false },
+    { label: 'direccion', property: 'direccion', type: 'text', visible: true },
+    {
+      label: 'direccion2',
+      property: 'direccion2',
+      type: 'text',
+      visible: false,
+    },
+    {
+      label: 'observacion',
+      property: 'observacion',
+      type: 'text',
+      visible: true,
+    },
+    { label: 'Actions', property: 'actions', type: 'button', visible: true },
+  ];
+  dataSource: MatTableDataSource<Cliente> | null;
 
   tableColumns: TableColumn<Order>[] = [
     {
-      label: '',
-      property: 'status',
-      type: 'badge'
+      label: "",
+      property: "status",
+      type: "badge",
     },
     {
-      label: 'PRODUCT',
-      property: 'name',
-      type: 'text'
+      label: "PRODUCT",
+      property: "name",
+      type: "text",
     },
     {
-      label: '$ PRICE',
-      property: 'price',
-      type: 'text',
-      cssClasses: ['font-medium']
+      label: "$ PRICE",
+      property: "price",
+      type: "text",
+      cssClasses: ["font-medium"],
     },
     {
-      label: 'DATE',
-      property: 'timestamp',
-      type: 'text',
-      cssClasses: ['text-secondary']
-    }
+      label: "DATE",
+      property: "timestamp",
+      type: "text",
+      cssClasses: ["text-secondary"],
+    },
   ];
   tableData = tableSalesData;
 
-  series: ApexAxisChartSeries = [{
-    name: 'Subscribers',
-    data: [28, 40, 36, 0, 52, 38, 60, 55, 67, 33, 89, 44]
-  }];
-
-  userSessionsSeries: ApexAxisChartSeries = [
+  series: ApexAxisChartSeries = [
     {
-      name: 'Users',
-      data: [10, 50, 26, 50, 38, 60, 50, 25, 61, 80, 40, 60]
-    },
-    {
-      name: 'Sessions',
-      data: [5, 21, 42, 70, 41, 20, 35, 50, 10, 15, 30, 50]
+      name: "Subscribers",
+      data: [28, 40, 36, 0, 52, 38, 60, 55, 67, 33, 89, 44],
     },
   ];
 
-  salesSeries: ApexAxisChartSeries = [{
-    name: 'Sales',
-    data: [28, 40, 36, 0, 52, 38, 60, 55, 99, 54, 38, 87]
-  }];
+  userSessionsSeries: ApexAxisChartSeries = [
+    {
+      name: "Users",
+      data: [10, 50, 26, 50, 38, 60, 50, 25, 61, 80, 40, 60],
+    },
+    {
+      name: "Sessions",
+      data: [5, 21, 42, 70, 41, 20, 35, 50, 10, 15, 30, 50],
+    },
+  ];
 
-  pageViewsSeries: ApexAxisChartSeries = [{
-    name: 'Page Views',
-    data: [405, 800, 200, 600, 105, 788, 600, 204]
-  }];
+  salesSeries: ApexAxisChartSeries = [
+    {
+      name: "Sales",
+      data: [28, 40, 36, 0, 52, 38, 60, 55, 99, 54, 38, 87],
+    },
+  ];
 
-  uniqueUsersSeries: ApexAxisChartSeries = [{
-    name: 'Unique Users',
-    data: [356, 806, 600, 754, 432, 854, 555, 1004]
-  }];
+  pageViewsSeries: ApexAxisChartSeries = [
+    {
+      name: "Page Views",
+      data: [405, 800, 200, 600, 105, 788, 600, 204],
+    },
+  ];
+
+  uniqueUsersSeries: ApexAxisChartSeries = [
+    {
+      name: "Unique Users",
+      data: [356, 806, 600, 754, 432, 854, 555, 1004],
+    },
+  ];
 
   uniqueUsersOptions = defaultChartOptions({
     chart: {
-      type: 'area',
-      height: 100
+      type: "area",
+      height: 100,
     },
-    colors: ['#ff9800']
+    colors: ["#ff9800"],
   });
 
   icGroup = icGroup;
@@ -86,20 +151,46 @@ export class DashboardAnalyticsComponent implements OnInit {
   icTimer = icTimer;
   icMoreVert = icMoreVert;
 
-  constructor(private cd: ChangeDetectorRef) { }
+  constructor(private cd: ChangeDetectorRef,  private apollo: Apollo) {}
 
-  ngOnInit() {
-    setTimeout(() => {
-      const temp = [
-        {
-          name: 'Subscribers',
-          data: [55, 213, 55, 0, 213, 55, 33, 55]
-        },
-        {
-          name: ''
-        }
-      ];
-    }, 3000);
+  get visibleColumns() {
+    return this.columns
+      .filter((column) => column.visible)
+      .map((column) => column.property);
   }
 
+  getData() {
+    this.data = this.apollo
+      .watchQuery<Query>({ query: GetClientes })
+      .valueChanges.pipe(map(({ data }) => data.cliente));
+    return this.data;
+  }
+
+  ngOnInit() {
+   
+      this.getData().subscribe((customers) => {
+        this.subject$.next(customers);
+        console.log(customers);
+      });
+  
+      this.dataSource = new MatTableDataSource();
+  
+      this.data$.pipe(filter<Cliente[]>(Boolean)).subscribe((customers) => {
+        this.customers = customers;
+        this.dataSource.data = customers;
+      });
+
+  
+    // setTimeout(() => {       
+      // const temp = [
+      //   {
+      //     name: "Subscribers",
+      //     data: [55, 213, 55, 0, 213, 55, 33, 55],
+      //   },
+      //   {
+      //     name: "",
+      //   },
+      // ];
+    // }, 3000);
+  }
 }
